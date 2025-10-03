9b8560485df75ac91192025c7bb655259d1235b3
pradhanhitesh <htshpradhan5@gmail.com>
2025-10-03 11:27:33 +0530

    refactor: add files
    - added version in __init__.py
    - added bumpversion.cfg

---
+++ mlproject/__init__.py
@@ -0,0 +1,8 @@
+"""MLProject package initialization."""
+
+__version__ = "0.1.0"
+
+# Optional: re-export CLI so people can run programmatically
+from .cli import cli
+
+__all__ = ["cli", "__version__"]
