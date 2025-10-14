dc233204c4565d7b0ff0be424c4494d44bd4d8e0
lab-calibra <calibra.sanscog@gmail.com>
2025-10-14 09:43:19 +0530

    fix: add support framework installation on Windows - Fixes #2 - Added packages in `framework.py` - Modified logic to handle instatllation on Windows

--- mlproject/frameworks.py
+++ mlproject/frameworks.py
@@ -6,12 +6,18 @@ FRAMEWORK_REQUIREMENTS = {
         "darwin": [
             "tensorflow-macos==2.15.0"
         ],
+        "windows": [
+            "tensorflow==2.15.0"
+        ],
     },
     "keras": {
         "linux": [
             "keras==3.3.0"
         ],
         "darwin": [
+            "keras==3.3.0"
+        ],
+        "windows": [
             "keras==3.3.0"
         ],
     },
@@ -24,6 +30,13 @@ FRAMEWORK_REQUIREMENTS = {
         "darwin": [
             "# To install PyTorch on macOS (CPU only, nightly):"
             "pip3 install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu"
+        ],
+        "windows": [
+            # Use the official PyTorch wheel index for Windows
+            "torch==2.1.2+cu121",
+            "torchvision==0.16.2+cu121",
+            "torchaudio==2.1.2+cu121",
+            "--index-url https://download.pytorch.org/whl/cu121"
         ],
     },
 }
